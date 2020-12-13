208ec7766b91da48ddcf0a69f767d692356dc5b5
vandrulfr <arturmelo@id.uff.br>
2020-12-13 09:55:57 -0300

    Testes atualizados

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import Clinica.ClinicaController;
 import Consulta.Consulta;
 import Consulta.ConsultaController;
 import Paciente.Paciente;
+import Paciente.PacienteController;
 import Psicologo.Psicologo;
 import Psicologo.PsicologoController;
 
@@ -14,10 +15,12 @@ public class Main {
         ClinicaController clinicas = new ClinicaController();
         PsicologoController psicologos = new PsicologoController();
         ConsultaController consultas = new ConsultaController();
+        PacienteController pacientes = new PacienteController();
         clinicas.criarClinica("Sao Goncalo", "Centro", "Rua Principal", "");
         clinicas.criarClinica("Sao Goncalo", "Apollo", "Rua Marginal", "");
         Clinica clinica = clinicas.getObject(0);
-        Paciente paciente = new Paciente("Paciente McPacienteface", 12345678912l);
+        pacientes.criarPaciente("Paciente McPacienteface", 12345678912l);
+        Paciente paciente = pacientes.getObject(0);
         psicologos.criarPsicologo("Psicologo McPsicologoface", clinica);
         Psicologo psicologo = psicologos.getObject(0);
         consultas.criarConsulta(paciente,psicologo);
